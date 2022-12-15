6aec64274a1e1e5b918665dd51f04caaf9f7e4df
StanleyEscobar99 <stanley.escobar.99@gmail.com>
2022-12-15 18:12:30 -0500

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+export default function Footer() {
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li><a class="bottomNav" href="#">Home</a></li>
+                    <li><a class="bottomNav" href="#">Women's</a></li>
+                    <li><a class="bottomNav" href="#">Mens's</a></li>
+                    <li><a class="bottomNav" href="#">On the Street</a></li>
+                    <li><a class="bottomNav" href="#">The catwalk</a></li>
+                    <li><a class="bottomNav" href="#">AdWatch</a></li>
+                    <li><a class="bottomNav" href="#">About</a></li>
+                    <li><a class="bottomNav" href="#">Tips</a></li>
+                </ul>
+            </nav>
+            <h4>&copy; 2013 Valet Industries, Inc</h4>
+        </footer>
+    )
+}
